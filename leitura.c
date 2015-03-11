e2c97066a49e8267ccdd98d93cde40c533ff7c20
Gstavo <gustavocg@live.com.pt>
2015-03-11 21:07:08 +0000

    Leitura bug fixs

--- leitura.c
+++ leitura.c
@@ -4,14 +4,74 @@
 #include <stdlib.h>
 
 #define MAX_LINE 40
+#define MAX 500000
+
+typedef struct compras {
+	char codigo_Produto[10];
+	double preco_unitario;
+	int unidades_compradas;
+	char tipo;	/*promoção-P;normal-N*/
+	char codigo_cliente[10];
+	int mes_compra;
+} Compras;
+
+typedef Compras *Comp;
+
+void printCompras(Compras a){
+	printf("CP = %s\n",a.codigo_Produto);
+	printf("Preço Unitario = %f\n",a.preco_unitario);
+	printf("Unidades Compradas = %d\n",a.unidades_compradas);
+	printf("Tipo = %c\n",a.tipo);
+	printf("CC = %s\n",a.codigo_cliente);
+	printf("Mes = %d\n",a.mes_compra);
+}
+
+void tokenizer(Comp a, int j, char linha[MAX_LINE]){
+	char* t; 
+	int x=0, n;
+	double m;
+	t=strtok(linha, " ");
+	while (t!=NULL) 
+	{
+        	if(x == 0) strcpy(a[j].codigo_Produto, t);
+        	if(x == 1) {
+			m=atof(t);
+			a[j].preco_unitario=m;
+		}
+        	if(x == 2) {
+			n=atoi(t);
+			a[j].unidades_compradas=n;
+		}
+        	if(x == 3) a[j].tipo = t[0];
+        	if(x == 4) strcpy(a[j].codigo_cliente, t); /* No name codigo_cliente */
+        	if(x == 5) {
+			n=atoi(t);
+			a[j].mes_compra=n;/* no name mes_compra em Compras*/
+		}
+        	x++;
+        	t=strtok(NULL, " ");
+	}	
+}
+
+void init(Comp array) {
+	int i;
+	for(i=0; i<MAX; i++) {
+		strncpy(array[i].codigo_Produto, "", 10);
+		array[i].preco_unitario=0.0;
+		array[i].unidades_compradas=0;
+		array[i].tipo=' ';
+		strncpy(array[i].codigo_cliente, "", 10);
+		array[i].mes_compra=0;
+	}
+}
 
 int main () {
-	int i=0, countC=0, countP=0,countCompras =0;
+	int i=0, countC=0, countP=0, countCompras=0;
 	FILE *clientes, *produtos, *fcompras;
-	char linha[MAX_LINE];
-	char** compras;
-	char** clnt;
-	char** prod;
+	char **compras, **clnt, **prod;
+	
+	Comp array=(Comp)malloc(MAX*sizeof(Compras));
+/*	init(array); desnecessário ????*/
 	
 	compras = calloc(500000,sizeof(char*));
 	for(i=0;i<500000;i++)
@@ -29,39 +89,33 @@ int main () {
 	produtos=fopen("produtos.txt","r");
 	fcompras=fopen("compras.txt","r");
 	
-	i=0;
-	while(!feof(clientes)) {
-		while(fgets(linha, MAX_LINE, clientes)!=NULL) {
-			strcpy(clnt[i], linha);
-			i++;
+	for(i=0;fgets(clnt[i], MAX_LINE, clientes);i++) 
 			countC++;
-		}
-	}
 	fclose(clientes);
-	i=0;
-	while(!feof(produtos)) {
-		while(fgets(linha, MAX_LINE, produtos)!=NULL) {
-			strcpy(prod[i], linha);
-			i++;
+	
+	for(i=0;fgets(prod[i], MAX_LINE, produtos);i++) 
 			countP++;
-		}
-	}
 	fclose(produtos);
-	i=0;
-	while(!feof(fcompras)){
-		while(fgets(linha, MAX_LINE,fcompras)!=NULL){
-			strcpy(compras[i],linha);
-			i++;
+	
+	for(i=0;fgets(compras[i], MAX_LINE,fcompras);i++){
+			tokenizer(array, i, compras[i]);
 			countCompras++;
 		}
-	}
 	fclose(fcompras);
-	printf("\nClientes=%d, Produtos=%d, Compras=%d\n\n", countC, countP,countCompras);
-	/* Imprime as primeiras 5 linhas da matriz*/
+	/*
+	printf("\nClientes=%d, Produtos=%d, Compras=%d\n\n", countC, countP, countCompras);
+	* Imprime as primeiras 5 linhas da matriz*
 	for(i=0; i<5; i++) {
-		printf("Cliente %d: %s\nProduto %d: %s\nCompra %d: %s\n\n", (i+1), clnt[i], (i+1), prod[i],i+1,compras[i]);
+		printf("Cliente %d: %sProduto %d: %sCompra %d: %s\n\n", (i+1), clnt[i], (i+1), prod[i],i+1,compras[i]);
 	}
-		
+	*/
+	/* Codigo imprime compras */
+	for(i=0;i<5;i++){
+		printf("Compra n %d\n\n",i+1);
+		printCompras(array[i]);
+		putchar('\n');
+	}
+
 	for(i=0;i<20000;i++)
 		free(clnt[i]);
 	free(clnt);
@@ -76,6 +130,7 @@ int main () {
 	
 	return 0;
 }
+	
 
 	
 	
