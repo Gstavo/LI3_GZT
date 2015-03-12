b7a24281e3e8b1b10a14da259709b0279dd62d3d
Gstavo <gustavocg@live.com.pt>
2015-03-12 17:32:24 +0000

    Updated leitura

--- leitura.c
+++ leitura.c
@@ -3,6 +3,7 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #define MAX_LINE 40
 #define MAX 500000
@@ -25,6 +26,50 @@ void printCompras(Compras a){
 	printf("Tipo = %c\n",a.tipo);
 	printf("CC = %s\n",a.codigo_cliente);
 	printf("Mes = %d\n",a.mes_compra);
+}
+
+int validaMes(int mes){
+	if(mes>=1 && mes<=12){return 1;}/*Verdadeiro*/
+	else return 0;/*Falso*/
+}
+
+int validaTipo(char a){
+	if(a=='P' || a=='N' || a=='p' || a=='n') {return 1;}/*Verdadeiro*/
+    else return 0;/*Falso*/
+}
+
+int validaUnidades(int unidade){
+	if(unidade>0 && unidade<200000){return 1;}/*Verdadeiro*/
+	else return 0;/*Falso*/
+}
+
+int validaPreco(double p){
+	if(p>0.0){return 1;}/*Verdadeiro*/
+	else return 0;/*Falso,seria gratis-oferta*/
+}
+
+int isdigitN(char a) {return ((a >= 48) && (a<=57));}
+
+int validaCC(char cc[]){
+	int i;
+	for(i=0;i<2;i++)
+		if (!isalpha(cc[i])) return 0;
+	for(;i<5;i++)
+		if (!isdigitN(cc[i])) return 0;
+	return 1;
+}
+
+int validaCP(char cc[]){
+	int i;
+	for(i=0;i<2;i++)
+		if (!isalpha(cc[i])) return 0;
+	for(;i<6;i++)
+		if (!isdigitN(cc[i])) return 0;
+	return 1;
+}
+
+int validateCompras(Compras a){
+	return ( validaMes(a.mes_compra) && validaTipo(a.tipo) && validaUnidades(a.unidades_compradas) && validaPreco(a.preco_unitario) && validaCP(a.codigo_Produto) && validaCC(a.codigo_cliente) );
 }
 
 void tokenizer(Comp a, int j, char linha[MAX_LINE]){
@@ -67,7 +112,7 @@ void init(Comp array) {
 }
 
 int main () {
-	int i=0, countC=0, countP=0, countCompras=0;
+	int i=0, countC=0, countP=0, countCompras=0,compras_invalidas=0;
 	FILE *clientes, *produtos, *fcompras;
 	char **compras, **clnt, **prod;
 	char linha[MAX_LINE];
@@ -109,9 +154,13 @@ int main () {
 	
 	for(i=0;fgets(linha, MAX_LINE,fcompras);i++)
 	{
+			int valido;
 			linha[strlen(linha)-1] = '\0';
 			strcpy(compras[i],linha);
 			tokenizer(array, i, compras[i]);
+			valido = validateCompras(array[i]);
+			if(!valido) {   
+           				compras_invalidas++;}
 			countCompras++;
 	}
 	fclose(fcompras);
@@ -129,6 +178,8 @@ int main () {
 		printCompras(array[i]);
 		putchar('\n');
 	}
+
+	printf("N compras invalidas %d de %d compras\n",compras_invalidas,countCompras);
 
 	for(i=0;i<20000;i++)
 		free(clnt[i]);
