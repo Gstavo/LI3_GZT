e3cc4f1bb743d4cd54a3411fa6d5b6a140002ec5
zecarlos94 <zecarlos@MBP-de-Jose.home>
2015-04-18 12:22:17 +0100

    Query 4 totalmente funcional como as restantes todas de navegação apresenta opção sair,próxima pagina e pagina anterior totalmente funcionais e com resultados correctos

--- main.c
+++ main.c
@@ -172,18 +172,31 @@ void query3(HashTable ht) {
 	}
 }
 
+void guardaNComp(HashTable ht,GrowingArray ga,GrowingArray resultado){
+	int j,r=0;
+	for(j=0;j<ga->size;j++){
+		if(!searchHash(ht,ga->Elems[j])){
+			resultado->Elems[r]=ga->Elems[j];
+			r++;
+		}
+	}
+}
+
 void query4(HashTable ht, AAVL cp) {
-   		int i,resposta,conta=0,paginas,total=0,quantos;
+   		int i,resposta,conta=0,paginas,total=0,quantos,index=0;
         GrowingArray ga = initGrowingArray(200000,ArrayString);
+        GrowingArray resultado = initGrowingArray(200000,ArrayString);
 		printf("\nLISTA DE PRODUTOS NAO COMPRADOS: \n");
         for(i=0;i<MAX_LETTERS;i++) guardArrayAVL(cp[i],ga,ArrayString);
         for(i=0;i < ga->size;i++) if(!searchHash(ht,ga->Elems[i])) {total++;}
-        printf("QUANTOS RESULTADOS PRETENDE VER AGORA?\n");
+        guardaNComp(ht,ga,resultado);
+		printf("QUANTOS RESULTADOS PRETENDE VER AGORA?\n");
 		if(scanf("%d", &quantos));
-        for(i=0;conta<quantos && i < ga->size;i++) {
-                if(!searchHash(ht,ga->Elems[i])) {
-			printf("%s\n",(char*)ga->Elems[i]);
+        for(i=0;(index + i) < ga->size && conta<quantos;) 
+        { 
+			printf("%s\n",(char*)resultado->Elems[index+i]);
 			conta++;
+			i++;
 			if(conta==quantos){
 				printf("\nESCOLHA UMA OPCAO?\n1-SAIR\n2-PROXIMO\n3-ANTERIOR\n\n");
 				if(scanf("%d", &resposta)) {
@@ -199,8 +212,7 @@ void query4(HashTable ht, AAVL cp) {
 				}
 			}
 		}
-	}
-	printf("\nNUMERO DE PRODUTOS QUE NINGUEM COMPROU: %d\n",total);
+		printf("\nNUMERO DE PRODUTOS QUE NINGUEM COMPROU: %d\n",total);
 }
 
 void query5(Contabilidade contClnt) {
